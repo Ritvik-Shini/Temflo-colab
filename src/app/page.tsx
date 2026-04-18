68138388577cc8f9d57313cf945e3e0c8e69c334
itsrichasingh <richa2003singh@gmail.com>
2026-04-18 11:21:41 +0530

    NEWWW

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,28 @@ import { Metadata } from "next";
 import Hero from '@/components/Home/Hero';
 import ThumbnailCarousel from '@/components/Home/Conferences';
 import WorkSpeakers from '@/components/Home/WorkSpeakers';
-import EventTicket from '@/components/Home/EventTicket';
 import Highlight from '@/components/Home/YearHighlight/page';
 import Upcoming from '@/components/Home/Upcoming';
-import Testimonials from '@/components/Home/Testimonials';
 import TicketSection from '@/components/Home/TicketSection';
+import Industries from '@/components/Home/industries/industriesweserve';
 export const metadata: Metadata = {
-  title: "Symposium",
+  title: "Temflo",
 };
 
 export default function Home() {
   return (
     <main>
+     
       <Hero />
       <ThumbnailCarousel/>
       <WorkSpeakers/>
-      <EventTicket/>
+      <Industries/>
       <Highlight/>
       <Upcoming/>
       <TicketSection/>
+
+    
+
     </main>
   )
 }
